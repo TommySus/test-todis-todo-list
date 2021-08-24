4a79352466d2a205357bdbe169e9abcb4796a05d
TommySus <tommysusanto77@gmail.com>
2021-08-24 11:01:13 +0700

    done

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
 const routes = require('./routes/index')
 const cors = require('cors')
 
@@ -13,7 +12,4 @@ app.use(express.json())
 app.use("/",routes)
 
 
-
-app.listen(process.env.PORT || PORT, ()=> {
-    console.log(`listening on PORT ${PORT}`)
-})
+module.exports = app
